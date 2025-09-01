2e3a0584ae44823edafa6ddd40cdbcb3db7bc7b3
Santiago Sánchez Esquivel <ssancheze@siesa.com>
2025-09-01 16:40:47 -0500

    SS - Configurate OrdersMicroservice

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
 
   const logger = new Logger('Orders-Main');
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      port: envs.port
+    }
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -16,7 +21,7 @@ async function bootstrap() {
     })
   );
 
-  await app.listen(envs.port);
+  await app.listen();
 
   logger.log(`Orders Microservices running on port ${envs.port}`)
 }
