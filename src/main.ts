ab272ce923a12d858231eee9a4cb9f21031d5dd1
Santiago Sánchez Esquivel <ssancheze@siesa.com>
2025-09-01 16:26:08 -0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
+
+async function bootstrap() {
+
+  const logger = new Logger('Orders-Main');
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
+  await app.listen(envs.port);
+
+  logger.log(`Orders Microservices running on port ${envs.port}`)
+}
+bootstrap();
